60d41411a56f6f45e8c5740474844fa46ded5e26
xkwm1 <48575811+xkwm1@users.noreply.github.com>
2021-02-01 12:05:00 -0800

    Create speed-object.cs

---
+++ speed-object.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speed / Timer: MonoBehaviour {
+
+    public float speed = 8f;
+    public float lifeDuration = 2f;
+
+    private float lifeTimer;
+
+    // Use this for initialization
+    void OnEnable () {
+        lifeTimer = lifeDuration;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        // Make the object move.
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        // Check if the object should be destoryed.
+        lifeTimer -= Time.deltaTime;
+        if (lifeTimer <= 0f) {
+            gameObject.SetActive (false);
+        }
+    }
+}
